a21d8d9dfe8653da84eb4faadc16c1d100c5eeab
jnthhn <jonathanh321@nycstudents.net>
2024-11-07 12:28:32 -0500

    s

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,1 +1,11 @@
 import "../styles/style.css";
+
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("steam")) {
+    document.body.classList.add("epicgames");
+    document.body.classList.remove("steam");
+  } else {
+    document.body.classList.add("steam");
+    document.body.classList.remove("epicgames");
+  }
+});
